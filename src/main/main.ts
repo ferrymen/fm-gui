83a2f0500a6733ec2cce3b1c95a479e657879e78
ferrymen <ferrymen33@gmail.com>
2019-01-19 11:51:10 +0800

    to typescript

---
+++ src/main/main.ts
@@ -0,0 +1,37 @@
+import { app, BrowserWindow } from "electron";
+import { resolve } from "path";
+
+let mainWindow: BrowserWindow | null;
+
+const createWindow = async () => {
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800,
+  });
+
+  if (process.env.NODE_ENV === "development") {
+    mainWindow.loadURL(`file://${resolve(__dirname, "../../", "static")}/index.html`);
+  } else {
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+  }
+
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+};
+
+app.on("ready", createWindow);
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
