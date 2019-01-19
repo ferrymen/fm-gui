1eddbe644b8ed7e4e1b00292cb33b685e5ca064e
ferrymen <ferrymen33@gmail.com>
2019-01-20 02:12:40 +0800

    debug main and renderer process

--- build/renderer/webpack.conf.dev.ts
+++ build/renderer/webpack.conf.dev.ts
@@ -1,16 +1,33 @@
 import { spawn } from "child_process";
-import { resolve } from "path";
-import webpack from "webpack";
+import FriendlyErrorsWebpackPlugin from "friendly-errors-webpack-plugin";
+import nodeNotifier from "node-notifier";
+import portfinder from "portfinder";
+import webpack, { Configuration } from "webpack";
 import merge from "webpack-merge";
+import packageConfig from "../../package.json";
+import shareConf from "../share/config";
 import confBase from "./webpack.conf.base";
 
-const publicPath = "http://localhost:8080/"; // Must to be "http://localhost:8080/", not "http://localhost:8080"
+const createNotifierCallback = () => {
+  return (severity: any, errors: any) => {
+    console.log(errors);
+    if (severity !== "error") {
+      return;
+    }
 
-export default merge.smart(confBase, {
+    const error = errors[0];
+
+    nodeNotifier.notify({
+      message: severity + ": " + error.name,
+      title: packageConfig.name,
+    });
+  };
+};
+
+const confDev: Configuration = merge.smart(confBase, {
   devServer: {
     // contentBase: resolve(__dirname, "../../", "dev"),
     hot: true,
-    publicPath,
     before() {
       if (process.env.RENDERER_PRE) {
         console.log("Luanching Main Process...");
@@ -27,7 +44,7 @@ export default merge.smart(confBase, {
   // devtool: "inline-source-map",
   entry: {
     // resolve(__dirname, "../../", "src/renderer/renderer.ts"),
-    renderer: [
+    [shareConf.base.entry]: [
       "./src/renderer/renderer.dev.tsx",
     ],
   },
@@ -58,11 +75,39 @@ export default merge.smart(confBase, {
       },
     ],
   },
-  output: {
-    publicPath, // webpack output is served from http://localhost:8080
-  },
   plugins: [
     new webpack.HotModuleReplacementPlugin(), // [HMR] Hot Module Replacement is disabled
   ],
   target: "electron-renderer",
+});
+
+export default new Promise((resolve, reject) => {
+  portfinder.basePort = Number(process.env.PORT) || shareConf.dev.port;
+  portfinder.getPortPromise().then(
+    (port: any) => {
+
+      process.env.PROTOCOL = process.env.PROTOCOL  || shareConf.dev.protocol;
+      // ！:error TS2532: Object is possibly 'undefined'.
+      confDev.devServer!.host = process.env.HOST = process.env.HOST || shareConf.dev.host;
+      confDev.devServer!.port = process.env.PORT = port;
+
+      // Must to be "http://localhost:8080/", not "http://localhost:8080"
+      const publicPath = `${process.env.PROTOCOL}://${process.env.HOST}:${process.env.PORT}/`;
+      confDev.devServer!.publicPath = confDev.output!.publicPath = process.env.PUBLIC_PATH  = publicPath;
+      process.env.ENTRY = shareConf.base.entry;
+
+      confDev.plugins!.push(new FriendlyErrorsWebpackPlugin({
+        compilationSuccessInfo: {
+          messages: [`Renderer process is running here: ${publicPath}`],
+          notes: ["Enjoy it..."],
+        },
+        onErrors: createNotifierCallback,
+      }));
+
+      resolve(confDev);
+    },
+    (err) => {
+      reject(err);
+    },
+  );
 });
---
+++ build/share/config.ts
@@ -0,0 +1,10 @@
+export default {
+  base: {
+    entry: "renderer",
+  },
+  dev: {
+    host: "localhost",
+    port: 8080,
+    protocol: "http",
+  },
+};
