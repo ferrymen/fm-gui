c8068c2a5b82c0beeb8467c34ff24e1c39ead3e5
ferrymen <ferrymen33@gmail.com>
2019-01-19 15:50:18 +0800

    webpack output is served from /dev

--- build/renderer/webpack.conf.dev.ts
+++ build/renderer/webpack.conf.dev.ts
@@ -3,9 +3,12 @@ import merge from "webpack-merge";
 import confBase from "./webpack.conf.base";
 
 export default merge.smart(confBase, {
+  devServer: {
+    contentBase: resolve(__dirname, "../../", "dev"),
+  },
   mode: "development",
-  // output: {
-  //   publicPath: `http://localhost:8080`, // webpack output is served from http://localhost:8080
-  // },
+  output: {
+    publicPath: "dev", // webpack output is served from http://localhost:8080
+  },
   target: "electron-renderer",
 });
