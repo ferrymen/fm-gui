b513ee8394a03538384edd950d71d38b76b2f658
ferrymen <ferrymen33@gmail.com>
2019-01-20 12:04:33 +0800

    add redux-logger

--- src/renderer/redux/store/store.dev.ts
+++ src/renderer/redux/store/store.dev.ts
@@ -1,6 +1,7 @@
 import { applyMiddleware, createStore, Reducer } from "redux";
 import thunk from "redux-thunk";
 import { IRootState, rootReducer } from "../reducer";
+import { createLogger } from "redux-logger";
 
 interface IDebugWindow extends Window {
   __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: (args: any) => any;
@@ -12,6 +13,13 @@ function configureStore(initialState?: IRootState) {
   const composeEnhancers = (window as IDebugWindow).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({});
 
   middlewares.push(thunk);
+
+  const logger = createLogger({
+    collapsed: true,
+    level: "info",
+  });
+  middlewares.push(logger);
+
   enhancers.push(applyMiddleware(...middlewares));
 
   const enhancer = composeEnhancers(...enhancers);
