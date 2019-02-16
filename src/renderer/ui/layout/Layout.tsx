9dec33db34dff09e20b9cf097d2495d63640614a
ferrymen <ferrymen33@gmail.com>
2019-02-16 11:35:44 +0800

    layout

--- src/renderer/ui/layout/Layout.tsx
+++ src/renderer/ui/layout/Layout.tsx
@@ -9,13 +9,44 @@ interface ILayoutProps {
 
 const LayoutBase: SFC<ILayoutProps & WithStyles<ClassKey> & WithTheme> = props => {
   const { classes, theme } = props;
+  // let size = undefined;
+  // let dragging = false;
+  // const handleDragStart = () => {
+  //   dragging = true;
+  // };
+  // const handleDragFinished = () => {
+  //   dragging = false;
+  //   setTimeout(() => {
+  //     size = undefined;
+  //   }, 0)
+  // };
+  // const handleDrag = (width: number) => {
+  //   if (width > 50 && width <= 100) {
+  //     size = 50;
+  //   } else {
+  //     size = undefined;
+  //   }
+  // };
+
   return (
-    <SplitPane split="vertical" defaultSize="20%" className="primary">
-      <div>left</div>
+    <SplitPane
+      split="vertical"
+      minSize={100}
+      maxSize={300}
+      defaultSize="20%"
+      // size={dragging ? undefined : size}
+      // onChange={handleDrag}
+      // onDragStarted={handleDragStart}
+      // onDragFinished={handleDragFinished}
+      className="primary">
+      <SplitPane split="vertical" maxSize={50}>
+        <div>side</div>
+        <div>left</div>
+      </SplitPane>
       <SplitPane split="horizontal" maxSize={50}>
         <div>header</div>
-        <SplitPane split="horizontal" defaultSize="80%">
-          <SplitPane split="vertical" defaultSize="80%">
+        <SplitPane split="horizontal" defaultSize="80%" maxSize={-50}>
+          <SplitPane split="vertical" defaultSize="80%" maxSize={-50}>
             <div>main</div>
             <div>right</div>
           </SplitPane>
