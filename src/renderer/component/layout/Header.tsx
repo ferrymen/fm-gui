a0ccbb6c53f8a5e67d21d18d87285902247952fd
ferrymen <ferrymen33@gmail.com>
2019-02-17 17:50:02 +0800

    header

--- src/renderer/component/layout/Header.tsx
+++ src/renderer/component/layout/Header.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
+import { ArrowBack } from "@material-ui/icons";
+import { injectIntl, InjectedIntl } from "react-intl";
 
-export const Header = () => {
+interface IProps {
+  intl: InjectedIntl;
+}
+
+const HeaderBase = (props: IProps) => {
+  const { intl } = props;
+
   return (
-    <div>
-      <Link to="/">
-        <button>返回</button>
-      </Link>
-    </div>
+    <AppBar position="static">
+      <Toolbar variant="dense">
+        <IconButton color="inherit" style={{paddingLeft: 0}} component={(itemProps: any) => <Link to="/" {...itemProps} />}>
+          <ArrowBack />
+        </IconButton>
+        <Typography variant="h6" color="inherit">
+          {intl.formatMessage({ id: "common.back" })}
+        </Typography>
+      </Toolbar>
+    </AppBar>
   )
 };
+
+export const Header = injectIntl(HeaderBase);
